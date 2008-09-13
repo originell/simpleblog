064017416783c895f3ff651b50d45ea058b1df5f
Luis Nell <de.da.de@originell.org>
2008-09-13 15:33:59 +0200

    - added url configs for entries (based upon james benett's coltrane-blog)

---
+++ urls/entries.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+from simpleblog.models import Entry
+
+entry_info_dict = {'queryset': Entry.objects.all(),
+                   'date_field': 'created',}
+
+urlpatterns = patterns('django.views.generic.date_based',
+    (r'^$', 'archive_index', entry_info_dict, 'archive_index'),
+    (r'^(?P<year>\d{4}/$', 'archive_year', entry_info_dict, 'archive_year'),
+    (r'^(?P<year>\d{4}/(?P<month>\w{3}/$', 'archive_month',
+        entry_info_dict, 'archive_month'),
+    (r'^(?P<year>\d{4}/(?P<month>\w{3}/(?P<day>\w{2}/$', 'archive_day',
+        entry_info_dict, 'archive_day'),
+    (r'^(?P<year>\d{4}/(?P<month>\w{3}/(?P<day>\w{2}/(?P<slug>[-\w]+)/$',
+        'entry_detail', entry_info_dict, 'entry_detail'),
+)
