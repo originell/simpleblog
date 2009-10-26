5f4cbb26d9fbca1b178584b257e2e8b7eb1e4533
Luis Nell <de.da.de@originell.org>
2009-10-26 12:51:54 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.rst')
+
+setup(
+    name = 'django_simpleblog',
+    version = '0.1',
+    url = 'http://github.com/originell/django_simpleblog',
+    description = 'A hopefully simple blog engine written with django',
+    long_description = README,
+
+    author = 'Luis Nell',
+    author_email = 'luis@originell.org',
+    packages = ['django_simpleblog',],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+   ]
+)
