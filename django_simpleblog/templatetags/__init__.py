2b4f6679fd9ec59c077fcc8c28ee0a2eea90521d
Luis Nell <de.da.de@originell.org>
2009-11-23 17:49:36 +0100

    added a templatetag which offers a humanized enumeration (f.e.: "one", "two" and "three")

--- django_simpleblog/templatetags/__init__.py
+++ django_simpleblog/templatetags/__init__.py
@@ -1,3 +1,4 @@
 from django.template import add_to_builtins
 
 add_to_builtins('django_simpleblog.templatetags.latest')
+add_to_builtins('django_simpleblog.templatetags.human_enum')
---
+++ django_simpleblog/templatetags/human_enum.py
@@ -0,0 +1,78 @@
+from django import template
+from django.db.models.query import QuerySet
+
+register = template.Library()
+
+class EnumNode(template.Node):
+    def __init__(self, queryset, urlify):
+        self.queryset = queryset
+        self.urlify = urlify
+
+    def __repr__(self):
+        return "<Human Enumeration Node>"
+
+    def render(self, context):
+        urlify = self.urlify
+        queryset = self.queryset
+        
+        output = ''
+        iter_len = queryset.count() - 2
+        i = 0
+        for item in queryset:
+            if i < iter_len:
+                if urlify:
+                    output += '"<a href="%s">%s</a>", ' % (item.get_absolute_url(),
+                                                           item)
+                else:
+                    output += '"%s", ' % item
+            elif iter_len == i:
+                if urlify:
+                    output += '"<a href="%s">%s</a>" and ' % (item.get_absolute_url(),
+                                                              item)
+                else:
+                    output += '"%s" and ' % item
+            else:
+                if urlify:
+                    output += '"<a href="%s">%s</a>"' % (item.get_absolute_url(),
+                                                         item)
+                else:
+                    output += '"%s"' % item
+            i += 1
+        return output
+
+class HumanEnumeration(object):
+    """
+    Creates a humanized enumeration of a specified queryset.
+
+    Usage::
+
+        {% human_enum YourQueryset %}
+
+    or if you want to have urls to the object
+
+        {% human_enum YourQueryset urlify %}
+
+    The __unicode__() method in your model should return some kind of title
+    of your object.
+    If you enable the option to build urls, your model needs a get_absolute_url()
+    method.
+    """
+    def __init__(self, tag_name):
+        self.tag_name = tag_name
+
+    def __call__(self, parser, token):
+        tokens = token.contents.split()
+        if len(tokens) < 2:
+            raise template.TemplateSyntaxError, \
+                "'%s' statement requires at least one argument" % self.tag_name
+
+        if type(tokens[1]) != QuerySet:
+            raise template.TemplateSyntaxError, \
+                "First argument in '%s' must be a QuerySet" % self.tag_name
+
+        urlify = False
+        if tokens[2] == 'urlify':
+            urlify = True
+        return EnumNode(queryset=tokens[1], urlify=urlify)
+
+register.tag('human_enum', HumanEnumeration('human_enum'))
