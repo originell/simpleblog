239e07a8876ee16d863aee5d4befdf767d78e89a
Luis Nell <luis@originell.org>
2010-04-10 13:08:48 +0200

    added categorical feed, migrated to feed settings based on a dictionary

--- django_simpleblog/feeds.py
+++ django_simpleblog/feeds.py
@@ -1,19 +1,33 @@
 from django.contrib.syndication.feeds import Feed
 from django.utils.feedgenerator import Atom1Feed
-from models import Entry
+from django.shortcuts import get_object_or_404
 from django.conf import settings
 
-set_title = getattr(settings, 'FEEDTITLE', 'Simpleblog latest 10 entries')
-set_descr = getattr(settings, 'FEEDDESCR', 'Simpleblog rss news feed')
-set_len = getattr(settings, 'FEEDLENGTH', 10)
+from models import Entry
+
+CATEGORIES = getattr(settings, 'CATEGORIES', True)
+if CATEGORIES:
+    from models import Category
+
+
+FEED_DEFAULTS = {'latest': {'title': 'Simpleblog Latest 10 Entries',
+                            'link':  '/news/',
+                            'description': 'Simpleblog RSS Newsfeed',
+                            'length': 10},}
+if CATEGORIES:
+    FEED_DEFAULTS['category'] = {'title': 'Simpleblog Categoryfeed %s',
+                                 'link': ''
+                                 'description': 'Feed For Category "%s"'}
+
+FEED_SETTINGS = getattr(settings, 'FEED_SETTINGS', FEED_DEFAULTS)
 
 class RSSLatestEntries(Feed):
-    title = set_title
-    link = '/news/'
-    description = set_descr
+    title = FEED_SETTINGS['latest']['title']
+    link = FEED_SETTINGS['latest']['link']
+    description = FEED_SETTINGS['latest']['description']
 
     def items(self):
-        return Entry.objects.order_by('-created')[:set_len]
+        return Entry.objects.order_by('-created')[:FEED_SETTINGS['latest']['length']
 
     def item_pubdate(self, item):
         return item.created
@@ -22,3 +36,16 @@ class AtomLatestEntries(RSSLatestEntries):
     feed_type = Atom1Feed
     subtitle = RSSLatestEntries.description
 
+if CATEGORIES:  
+    class RSSCategoryFeed(Feed):
+        def get_object(self, request, category_name):
+            return get_object_or_404(Category, name=category_name)
+
+        def title(self, item):
+            return FEED_SETTINGS['category']['title'] % item.name
+
+        def description(self, item):
+            return FEED_SETTINGS['category']['description'] % item.name
+
+        def items(self, item):
+            return Entry.objects.filter(category=category_name).order_by('-created')
