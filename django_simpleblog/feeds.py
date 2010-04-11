e60343349a5f69a112150d778332e1b05d50a999
Luis Nell <luis@originell.org>
2010-04-11 16:53:16 +0200

    added category based feeds for rss and atom

--- django_simpleblog/feeds.py
+++ django_simpleblog/feeds.py
@@ -1,22 +1,23 @@
-from django.contrib.syndication.feeds import Feed
+from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Atom1Feed
 from django.shortcuts import get_object_or_404
+from django.template.defaultfilters import truncatewords
 from django.conf import settings
 
 from models import Entry
-
 CATEGORIES = getattr(settings, 'CATEGORIES', True)
 if CATEGORIES:
     from models import Category
 
 
 FEED_DEFAULTS = {'latest': {'title': 'Simpleblog Latest 10 Entries',
-                            'link':  '/news/',
-                            'description': 'Simpleblog RSS Newsfeed',
-                            'length': 10},}
+                            'link':  '/latest/',
+                            'description': 'Simpleblog Newsfeed',
+                            'length': 10,
+                            'item_descr_length': 48,}
 if CATEGORIES:
-    FEED_DEFAULTS['category'] = {'title': 'Simpleblog Categoryfeed %s',
-                                 'link': ''
+    FEED_DEFAULTS['category'] = {'title': 'Simpleblog Categoryfeed',
+                                 'link': '/category/',
                                  'description': 'Feed For Category "%s"'}
 
 FEED_SETTINGS = getattr(settings, 'FEED_SETTINGS', FEED_DEFAULTS)
@@ -27,25 +28,49 @@ class RSSLatestEntries(Feed):
     description = FEED_SETTINGS['latest']['description']
 
     def items(self):
-        return Entry.objects.order_by('-created')[:FEED_SETTINGS['latest']['length']
+        return Entry.objects.order_by('-created')[:FEED_SETTINGS['latest']['length']]
 
     def item_pubdate(self, item):
         return item.created
+
+    def item_description(self, item):
+        return truncatewords(item.body, FEED_SETTINGS['latest']['item_descr_length'])
 
 class AtomLatestEntries(RSSLatestEntries):
     feed_type = Atom1Feed
     subtitle = RSSLatestEntries.description
 
-if CATEGORIES:  
+if CATEGORIES:
     class RSSCategoryFeed(Feed):
         def get_object(self, request, category_name):
             return get_object_or_404(Category, name=category_name)
 
-        def title(self, item):
-            return FEED_SETTINGS['category']['title'] % item.name
+        def title(self, obj):
+            try:
+                return FEED_SETTINGS['category']['title'] % obj.name
+            except TypeError:
+                return FEED_SETTINGS['category']['title']
 
-        def description(self, item):
-            return FEED_SETTINGS['category']['description'] % item.name
+        def link(self, obj):
+            return FEED_SETTINGS['category']['link']
 
-        def items(self, item):
-            return Entry.objects.filter(category=category_name).order_by('-created')
+        def description(self, obj):
+            try:
+                return FEED_SETTINGS['category']['description'] % obj.name
+            except TypeError:
+                return FEED_SETTINGS['category']['description']
+
+        def items(self, obj):
+            return Entry.objects.filter(category=obj).order_by('-created')
+
+        def item_pubdate(self, item):
+            return item.created
+
+        def item_description(self, item):
+            return truncatewords(item.body, FEED_SETTINGS['latest']['item_descr_length'])
+
+    class AtomCategoryFeed(RSSCategoryFeed):
+        feed_type = Atom1Feed
+
+        def subtitle(self, obj):
+            return super(AtomCategoryFeed, self).description(obj)
