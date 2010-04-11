a9991bcb56a9a711ca768cfd8b62d470d8e7df03
Luis Nell <luis@originell.org>
2010-04-11 16:53:53 +0200

    restructured feeds.py to support category based feeds and django >= 1.2

--- django_simpleblog/urls/feeds.py
+++ django_simpleblog/urls/feeds.py
@@ -1,12 +1,21 @@
 from django.conf.urls.defaults import *
-from django_simpleblog.feeds import RSSLatestEntries, AtomLatestEntries
+from django.conf import settings
+from django_simpleblog.feeds import *
 
-feeds = {
-    'rss': RSSLatestEntries,
-    'atom': AtomLatestEntries,
-}
+CATEGORIES = getattr(settings, 'CATEGORIES', True)
+if CATEGORIES:
+    urlpatterns = patterns('',
+        (r'^category/(?P<category_name>\w+)/rss/$', RSSCategoryFeed()),
+        (r'^category/(?P<category_name>\w+)/atom/$', AtomCategoryFeed()),
+    )
 
-urlpatterns = patterns('',
-    (r'^(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
-        {'feed_dict': feeds}),
+
+default_pattern = patterns('',
+        (r'^latest/rss/$', RSSLatestEntries),
+        (r'^latest/atom/$', AtomLatestEntries),
 )
+
+try:
+    urlpatterns += default_pattern
+except NameError:
+    urlpatterns = default_pattern
